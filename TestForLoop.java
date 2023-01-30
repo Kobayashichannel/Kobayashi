c6bfe04cfc78a6a29fe08074a58e718e85c91d5b
古见同学有交流\asus <Ppiano_Channel@outlook.com>
2023-01-30 23:51:30 +0800

    no message

---
+++ TestForLoop.java
@@ -0,0 +1,9 @@
+public class TestForLoop {
+	public static void main(String[] args) {
+		
+		for(int x = 10;x < 20;x=x+1) {
+			System.out.println("xµÄÖµÊÇ" + x);
+			System.out.println("\n");
+		}
+	}
+}
