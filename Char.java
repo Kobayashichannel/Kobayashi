9d79258c0e13d5697e127568dbed620e27a45e7c
古见同学有交流\asus <Ppiano_Channel@outlook.com>
2023-02-07 22:08:09 +0800

    test

---
+++ Char.java
@@ -0,0 +1,15 @@
+public class Char{
+	public static void main(String[] args) {
+	char expectedGrade = 'A';
+	char expectedGrade2 = 'B';
+	char expectedGrade3 = 'C';
+	char expectedGrade4 = 'D';
+	char expectedGrade5 = 'F';
+	System.out.println(expectedGrade);
+	System.out.println(expectedGrade2);
+	System.out.println(expectedGrade3);
+	System.out.println(expectedGrade4);
+	System.out.println(expectedGrade5);
+	}
+	
+}
