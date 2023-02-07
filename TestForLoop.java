ccdf13ba8b287b43d4500dff1e05bd66083ee0aa
Kobayashichannel <123864841+Kobayashichannel@users.noreply.github.com>
2023-02-07 21:45:18 +0800

    Delete TestForLoop.java

--- TestForLoop.java
+++
@@ -1,9 +0,0 @@
-public class TestForLoop {
-	public static void main(String[] args) {
-		
-		for(int x = 10;x < 20;x=x+1) {
-			System.out.println("xµÄÖµÊÇ" + x);
-			System.out.println("\n");
-		}
-	}
-}
