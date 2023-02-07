cbdac04e6d11d47c3167a5934b1cb2d5996afe42
古见同学有交流\asus <Ppiano_Channel@outlook.com>
2023-02-08 01:33:52 +0800

    test

---
+++ IntDetail.java
@@ -0,0 +1,13 @@
+
+
+public class IntDetail {
+	
+	//编写一个main方法
+	public static void main(String[] args){
+	
+	  //Java的整数常量（具体值）默认为int 型,声明long型常量须后加"l"或者"L"
+	  int n1 = 1; //4个字节
+	  //int n2 = 1L; //对不对?不对
+	  long n2 = 1L;//对
+	}
+}
