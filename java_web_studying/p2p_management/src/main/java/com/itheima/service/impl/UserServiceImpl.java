145cf7f8ea3842f95cf0dd188b38c2d060289858
Kobayashichannel <123864841+Kobayashichannel@users.noreply.github.com>
2023-11-08 05:05:38 +0800

    更新代码

--- java_web_studying/p2p_management/src/main/java/com/itheima/service/impl/UserServiceImpl.java
+++ java_web_studying/p2p_management/src/main/java/com/itheima/service/impl/UserServiceImpl.java
@@ -1,9 +1,9 @@
 package com.itheima.service.impl;
 
 import com.itheima.dao.IUserDAO;
-import com.itheima.dao.impl.UserDAOImpl;
 import com.itheima.domain.User;
 import com.itheima.service.IUserService;
+import com.itheima.utils.BeanFactory;
 
 public class UserServiceImpl implements IUserService {
 
@@ -11,10 +11,13 @@ public class UserServiceImpl implements IUserService {
 	@Override
 	public User login(String username, String password) {
 
-		IUserDAO userDao = new UserDAOImpl();
+		String path = this.getClass().getClassLoader().getResource("/bean.xml").getFile();
+
+		IUserDAO userDao = (IUserDAO) BeanFactory.createBean("userDao", path);
 		try {
 			return userDao.findUserByUsernameAndPassword(username, password);
 		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		return null;
